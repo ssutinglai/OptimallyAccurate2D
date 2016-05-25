3e8ab6cfe570119e3b5d3e41be390eadc0af4b00
seismobassoon <seismobassoon@gmail.com>
2016-05-25 22:50:50 +0200

    PLM challenging

--- fortran/opt2DPSVPLM.f90
+++ fortran/opt2DPSVPLM.f90
@@ -9,44 +9,44 @@ program opt22
   
   implicit none
   integer maxnz
-  parameter ( maxnz = 1000 )
+  parameter ( maxnz = 800 )
   !
   ! parameters for the gridding
-  real*8 dt,dx,dz
+  double precision dt,dx,dz
   ! parameters for the wavefield
   integer nt,nx,nz,it,ist,isx,isz
-  real*8 ux(maxnz+1,maxnz+1),uz(maxnz+1,maxnz+1)
-  real*8 ux1(maxnz+1,maxnz+1),ux2(maxnz+1,maxnz+1)
-  real*8 uz1(maxnz+1,maxnz+1),uz2(maxnz+1,maxnz+1)
-  real*8  e1(maxnz+1,maxnz+1), e2(maxnz+1,maxnz+1)
-  real*8  e3(maxnz+1,maxnz+1), e4(maxnz+1,maxnz+1)
-  real*8  e5(maxnz+1,maxnz+1), e6(maxnz+1,maxnz+1)
-  real*8  e7(maxnz+1,maxnz+1), e8(maxnz+1,maxnz+1)
-  real*8 e13(maxnz+1,maxnz+1),e14(maxnz+1,maxnz+1)
-  real*8 e15(maxnz+1,maxnz+1),e16(maxnz+1,maxnz+1)
-  real*8 e17(maxnz+1,maxnz+1),e18(maxnz+1,maxnz+1)
-  real*8 e19(maxnz+1,maxnz+1),e20(maxnz+1,maxnz+1)
-  real*8  f1(maxnz+1,maxnz+1), f2(maxnz+1,maxnz+1)
-  real*8  f3(maxnz+1,maxnz+1), f4(maxnz+1,maxnz+1)
-  real*8  f5(maxnz+1,maxnz+1), f6(maxnz+1,maxnz+1)
-  real*8  f7(maxnz+1,maxnz+1), f8(maxnz+1,maxnz+1)
-  real*8 f13(maxnz+1,maxnz+1),f14(maxnz+1,maxnz+1)
-  real*8 f15(maxnz+1,maxnz+1),f16(maxnz+1,maxnz+1)
-  real*8 f17(maxnz+1,maxnz+1),f18(maxnz+1,maxnz+1)
-  real*8 f19(maxnz+1,maxnz+1),f20(maxnz+1,maxnz+1)
-  real*8 work(maxnz+1,32)
+  double precision ux(maxnz+1,maxnz+1),uz(maxnz+1,maxnz+1)
+  double precision ux1(maxnz+1,maxnz+1),ux2(maxnz+1,maxnz+1)
+  double precision uz1(maxnz+1,maxnz+1),uz2(maxnz+1,maxnz+1)
+  double precision  e1(maxnz+1,maxnz+1), e2(maxnz+1,maxnz+1)
+  double precision  e3(maxnz+1,maxnz+1), e4(maxnz+1,maxnz+1)
+  double precision  e5(maxnz+1,maxnz+1), e6(maxnz+1,maxnz+1)
+  double precision  e7(maxnz+1,maxnz+1), e8(maxnz+1,maxnz+1)
+  double precision e13(maxnz+1,maxnz+1),e14(maxnz+1,maxnz+1)
+  double precision e15(maxnz+1,maxnz+1),e16(maxnz+1,maxnz+1)
+  double precision e17(maxnz+1,maxnz+1),e18(maxnz+1,maxnz+1)
+  double precision e19(maxnz+1,maxnz+1),e20(maxnz+1,maxnz+1)
+  double precision  f1(maxnz+1,maxnz+1), f2(maxnz+1,maxnz+1)
+  double precision  f3(maxnz+1,maxnz+1), f4(maxnz+1,maxnz+1)
+  double precision  f5(maxnz+1,maxnz+1), f6(maxnz+1,maxnz+1)
+  double precision  f7(maxnz+1,maxnz+1), f8(maxnz+1,maxnz+1)
+  double precision f13(maxnz+1,maxnz+1),f14(maxnz+1,maxnz+1)
+  double precision f15(maxnz+1,maxnz+1),f16(maxnz+1,maxnz+1)
+  double precision f17(maxnz+1,maxnz+1),f18(maxnz+1,maxnz+1)
+  double precision f19(maxnz+1,maxnz+1),f20(maxnz+1,maxnz+1)
+  double precision work(maxnz+1,32)
   ! parameter for the structure
-  !real*8 rrho(6),llam(6),mmu(6)
+  !double precision rrho(6),llam(6),mmu(6)
   character(80) :: vpfile, vsfile, rhofile
-  real*8 rho(maxnz+1,maxnz+1)
-  real*8 lam(maxnz+1,maxnz+1),mu(maxnz+1,maxnz+1)
-  real*8 vs(maxnz+1,maxnz+1),vp(maxnz+1,maxnz+1)
+  double precision :: rho(maxnz+1,maxnz+1)
+  double precision :: lam(maxnz+1,maxnz+1),mu(maxnz+1,maxnz+1)
+  double precision :: vs(maxnz+1,maxnz+1),vp(maxnz+1,maxnz+1)
   
-  real*8 Courant_number
+  double precision Courant_number
   ! parameter for the receiver
   integer nrx,nrz
   ! parameter for the waveform
-  real*8 t
+  double precision t
   !parameter for video
   real,dimension(maxnz+1,maxnz+1) :: snapux,snapuz
   integer, parameter :: IT_DISPLAY = 10
@@ -66,8 +66,50 @@ program opt22
   integer, parameter :: NPOINTS_PML = 10
 
   ! Ricker wavelets source
-  real*8 f0,t0
-  !real*8 tp,ts
+  double precision f0,t0
+  !double precision tp,ts
+
+
+  ! for evolution of total energy in the medium
+  double precision epsilon_xx,epsilon_yy,epsilon_xy
+  double precision, dimension(NSTEP) :: total_energy_kinetic,total_energy_potential
+  
+  ! power to compute d0 profile
+  double precision, parameter :: NPOWER = 2.d0
+
+  double precision, parameter :: K_MAX_PML = 1.d0 ! from Gedney page 8.11
+  double precision, parameter :: ALPHA_MAX_PML = 2.d0*PI*(f0/2.d0) ! from Festa and Vilotte
+  
+
+  ! could declare these arrays in PML only to save a lot of memory, but proof of concept only here
+  double precision, dimension(1:maxnz+1,1:maxnz+1) :: &
+      memory_dvx_dx, &
+      memory_dvx_dy, &
+      memory_dvy_dx, &
+      memory_dvy_dy, &
+      memory_dsigmaxx_dx, &
+      memory_dsigmayy_dy, &
+      memory_dsigmaxy_dx, &
+      memory_dsigmaxy_dy
+
+  double precision :: &
+      value_dvx_dx, &
+      value_dvx_dy, &
+      value_dvy_dx, &
+      value_dvy_dy, &
+      value_dsigmaxx_dx, &
+      value_dsigmayy_dy, &
+      value_dsigmaxy_dx, &
+      value_dsigmaxy_dy
+
+  ! 1D arrays for the damping profiles
+  double precision, dimension(NX) :: d_x,K_x,alpha_x,a_x,b_x,d_x_half,K_x_half,alpha_x_half,a_x_half,b_x_half
+  double precision, dimension(NY) :: d_y,K_y,alpha_y,a_y,b_y,d_y_half,K_y_half,alpha_y_half,a_y_half,b_y_half
+
+  double precision :: thickness_PML_x,thickness_PML_y,xoriginleft,xoriginright,yoriginbottom,yorigintop
+  double precision :: Rcoef,d0_x,d0_y,xval,yval,abscissa_in_PML,abscissa_normalized
+  
+
 
   character(120) :: commandline
   
@@ -202,9 +244,9 @@ end program opt22
 subroutine pinput( maxnz,nt,nx,nz,dt,dx,dz,vpfile,vsfile,rhofile,f0,t0,nrx,nrz )
   
   implicit none
-  real*8 f0,t0
+  double precision f0,t0
   integer maxnz,nt,nx,nz,nrx,nrz
-  real*8 dt,dx,dz !rho(*),lam(*),mu(*),tp,ts
+  double precision dt,dx,dz !rho(*),lam(*),mu(*),tp,ts
   character*80 tmpfile,dummy
   character*80 vpfile,vsfile,rhofile
   tmpfile='tmpfileforwork'
@@ -247,7 +289,7 @@ end subroutine pinput
 subroutine datainit( nx,nz,ux )
   
   integer nx,nz
-  real*8 ux(nx+1,*)
+  double precision ux(nx+1,*)
   integer i,j
   
   do j=1,nz+1
@@ -263,10 +305,10 @@ end subroutine datainit
 subroutine calstruct( maxnz,file2d,dx,dz,nx,nz,rho )
   implicit none
   integer maxnz,nx,nz
-  real*8 dx,dz,rho(maxnz+1,*)
+  double precision dx,dz,rho(maxnz+1,*)
   real(kind(1.0)) rrho(maxnz+1,maxnz+1)
   integer i,j,k,nox(6),noz(6)
-  real*8 x,z,xmax,zmax,trho,coef1,coef2
+  double precision x,z,xmax,zmax,trho,coef1,coef2
   integer recl_size
   character*80 file2d
   recl_size=kind(1.0)*(nx+1)*(nz+1)
@@ -283,8 +325,8 @@ subroutine calstruct2(maxnz,nx,nz,rho,vp,vs,lam,mu)
   implicit none
   
   integer i,j,maxnz,nx,nz
-  real*8 rho(maxnz+1,maxnz+1),vp(maxnz+1,maxnz+1),vs(maxnz+1,maxnz+1)
-  real*8 lam(maxnz+1,maxnz+1),mu(maxnz+1,maxnz+1)
+  double precision rho(maxnz+1,maxnz+1),vp(maxnz+1,maxnz+1),vs(maxnz+1,maxnz+1)
+  double precision lam(maxnz+1,maxnz+1),mu(maxnz+1,maxnz+1)
 
   do i=1,nx+1
      do j=1,nz+1
@@ -302,22 +344,22 @@ subroutine cales( maxnz,nx,nz,rho,lam,mu,dt,dx,dz,e1, e2, e3, e4, e5, e6, e7, e8
      f13,f14,f15,f16,f17,f18,f19,f20 )
   
   integer maxnz,nx,nz
-  real*8 rho(maxnz+1,*),lam(maxnz+1,*),mu(maxnz+1,*)
-  real*8 dt,dx,dz
-  real*8  e1(maxnz+1,*), e2(maxnz+1,*), e3(maxnz+1,*)
-  real*8  e4(maxnz+1,*), e5(maxnz+1,*), e6(maxnz+1,*)
-  real*8  e7(maxnz+1,*), e8(maxnz+1,*)
-  real*8 e13(maxnz+1,*),e14(maxnz+1,*),e15(maxnz+1,*)
-  real*8 e16(maxnz+1,*),e17(maxnz+1,*),e18(maxnz+1,*)
-  real*8 e19(maxnz+1,*),e20(maxnz+1,*)
-  real*8  f1(maxnz+1,*), f2(maxnz+1,*), f3(maxnz+1,*)
-  real*8  f4(maxnz+1,*), f5(maxnz+1,*), f6(maxnz+1,*)
-  real*8  f7(maxnz+1,*), f8(maxnz+1,*)
-  real*8 f13(maxnz+1,*),f14(maxnz+1,*),f15(maxnz+1,*)
-  real*8 f16(maxnz+1,*),f17(maxnz+1,*),f18(maxnz+1,*)
-  real*8 f19(maxnz+1,*),f20(maxnz+1,*)
+  double precision rho(maxnz+1,*),lam(maxnz+1,*),mu(maxnz+1,*)
+  double precision dt,dx,dz
+  double precision  e1(maxnz+1,*), e2(maxnz+1,*), e3(maxnz+1,*)
+  double precision  e4(maxnz+1,*), e5(maxnz+1,*), e6(maxnz+1,*)
+  double precision  e7(maxnz+1,*), e8(maxnz+1,*)
+  double precision e13(maxnz+1,*),e14(maxnz+1,*),e15(maxnz+1,*)
+  double precision e16(maxnz+1,*),e17(maxnz+1,*),e18(maxnz+1,*)
+  double precision e19(maxnz+1,*),e20(maxnz+1,*)
+  double precision  f1(maxnz+1,*), f2(maxnz+1,*), f3(maxnz+1,*)
+  double precision  f4(maxnz+1,*), f5(maxnz+1,*), f6(maxnz+1,*)
+  double precision  f7(maxnz+1,*), f8(maxnz+1,*)
+  double precision f13(maxnz+1,*),f14(maxnz+1,*),f15(maxnz+1,*)
+  double precision f16(maxnz+1,*),f17(maxnz+1,*),f18(maxnz+1,*)
+  double precision f19(maxnz+1,*),f20(maxnz+1,*)
   integer ix,iz
-  real*8 dt2,dx2,dz2,dxdz
+  double precision dt2,dx2,dz2,dxdz
   
   dt2 = dt * dt
   dx2 = dx * dx
@@ -427,11 +469,11 @@ end subroutine cales
 
 subroutine calf( maxnz,it,t,ist,isx,isz,dt,dx,dz,rho,tp,ts,fx,fz )
 
-  real*8 pi
+  double precision pi
   parameter ( pi=3.1415926535897932d0 )
   integer maxnz,it,ist,isx,isz
-  real*8 t,dt,dx,dz,rho,tp,ts,fx(maxnz+1,*),fz(maxnz+1,*)
-  real*8 b
+  double precision t,dt,dx,dz,rho,tp,ts,fx(maxnz+1,*),fz(maxnz+1,*)
+  double precision b
   
   if ( it.le.ist ) then
      b = pi * ( t - ts ) / tp
@@ -458,11 +500,11 @@ end subroutine calf
 subroutine calf2( maxnz,it,t,ist,isx,isz,dt,dx,dz,rho,f0,t0,fx,fz )
 
   implicit none
-  real*8 pi
+  double precision pi
   parameter ( pi=3.1415926535897932d0 )
   integer maxnz,it,ist,isx,isz
-  real*8 t,dt,dx,dz,rho,f0,t0,fx(maxnz+1,*),fz(maxnz+1,*)
-  real*8 b,a,factor
+  double precision t,dt,dx,dz,rho,f0,t0,fx(maxnz+1,*),fz(maxnz+1,*)
+  double precision b,a,factor
   
   factor=1.d3
   
@@ -503,27 +545,27 @@ subroutine calstep( maxnz,nx,nz, &
      work9,work10,work11,work12,optimise )
 
   integer maxnz,nx,nz,isx,isz
-  real*8 ux(maxnz+1,*),ux1(maxnz+1,*),ux2(maxnz+1,*)
-  real*8 uz(maxnz+1,*),uz1(maxnz+1,*),uz2(maxnz+1,*)
-  real*8  e1(maxnz+1,*), e2(maxnz+1,*), e3(maxnz+1,*)
-  real*8  e4(maxnz+1,*), e5(maxnz+1,*), e6(maxnz+1,*)
-  real*8  e7(maxnz+1,*), e8(maxnz+1,*)
-  real*8 e13(maxnz+1,*),e14(maxnz+1,*),e15(maxnz+1,*)
-  real*8 e16(maxnz+1,*),e17(maxnz+1,*),e18(maxnz+1,*)
-  real*8 e19(maxnz+1,*),e20(maxnz+1,*)
-  real*8  f1(maxnz+1,*), f2(maxnz+1,*), f3(maxnz+1,*)
-  real*8  f4(maxnz+1,*), f5(maxnz+1,*), f6(maxnz+1,*)
-  real*8  f7(maxnz+1,*), f8(maxnz+1,*)
-  real*8 f13(maxnz+1,*),f14(maxnz+1,*),f15(maxnz+1,*)
-  real*8 f16(maxnz+1,*),f17(maxnz+1,*),f18(maxnz+1,*)
-  real*8 f19(maxnz+1,*),f20(maxnz+1,*)
-  real*8 fx(maxnz+1,*),fz(maxnz+1,*)
-  real*8 work1(maxnz+1,-2:1),work2(maxnz+1,-1:2)
-  real*8 work3(maxnz+1,-2:1),work4(maxnz+1,-1:2)
-  real*8 work5(*),work6(maxnz+1,0:1)
-  real*8 work7(*),work8(maxnz+1,0:1)
-  real*8 work9(*),work10(maxnz+1,-2:1)
-  real*8 work11(*),work12(maxnz+1,-1:2)
+  double precision ux(maxnz+1,*),ux1(maxnz+1,*),ux2(maxnz+1,*)
+  double precision uz(maxnz+1,*),uz1(maxnz+1,*),uz2(maxnz+1,*)
+  double precision  e1(maxnz+1,*), e2(maxnz+1,*), e3(maxnz+1,*)
+  double precision  e4(maxnz+1,*), e5(maxnz+1,*), e6(maxnz+1,*)
+  double precision  e7(maxnz+1,*), e8(maxnz+1,*)
+  double precision e13(maxnz+1,*),e14(maxnz+1,*),e15(maxnz+1,*)
+  double precision e16(maxnz+1,*),e17(maxnz+1,*),e18(maxnz+1,*)
+  double precision e19(maxnz+1,*),e20(maxnz+1,*)
+  double precision  f1(maxnz+1,*), f2(maxnz+1,*), f3(maxnz+1,*)
+  double precision  f4(maxnz+1,*), f5(maxnz+1,*), f6(maxnz+1,*)
+  double precision  f7(maxnz+1,*), f8(maxnz+1,*)
+  double precision f13(maxnz+1,*),f14(maxnz+1,*),f15(maxnz+1,*)
+  double precision f16(maxnz+1,*),f17(maxnz+1,*),f18(maxnz+1,*)
+  double precision f19(maxnz+1,*),f20(maxnz+1,*)
+  double precision fx(maxnz+1,*),fz(maxnz+1,*)
+  double precision work1(maxnz+1,-2:1),work2(maxnz+1,-1:2)
+  double precision work3(maxnz+1,-2:1),work4(maxnz+1,-1:2)
+  double precision work5(*),work6(maxnz+1,0:1)
+  double precision work7(*),work8(maxnz+1,0:1)
+  double precision work9(*),work10(maxnz+1,-2:1)
+  double precision work11(*),work12(maxnz+1,-1:2)
   integer ix,iz,iz1,iz2,ix11,ix12,ix21,ix22
   logical optimise
 
