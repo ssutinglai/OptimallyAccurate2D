7c0366cd58fde5da9950b576eaeb076cf54b4f1c
seismobassoon <seismobassoon@gmail.com>
2016-06-06 06:30:15 +0200

    cleaning the problem

--- defparamcpml1d.m
+++
@@ -1,5 +0,0 @@
-function [sigpmlxa] = defparamcpml1d(ax,vm,npml)
-
-sigpmlxa = defparamcpml(ax,vm,npml);
-%sigpmlxb = defparamcpml(ax(1:end-1),vm,npml);
-end
--- fortran/compile.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-
-gfortran generator.f90 -o generator
-gfortran generator_elipsoid.f90 -o generator_elipsoid
