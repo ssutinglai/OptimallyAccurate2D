eda26e893210318735b47c9b72e9ba6fdab8952e
seismobassoon <seismobassoon@gmail.com>
2016-06-07 16:07:48 +0200

    some belahi's code

---
+++ src/frechetKernel.f90
@@ -0,0 +1,22 @@
+program frechetKernel
+
+  
+
+  ! Computation of Frechet derivatives for Vp and Vs
+  ! Strain wavefields should be calculated beforehand with OPT2D.f90
+  !
+
+  !
+  !
+  !                                            2016.6. N. Fuji (IPGP)
+  !
+  !
+
+
+  use parameters
+  implicit none
+
+  call paramMultiReader
+
+end program frechetKernel
+  
