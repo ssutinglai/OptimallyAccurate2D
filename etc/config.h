f3e684ce570dd6b19904d13279e80975f6408136
Nobuaki Fuji <seismobassoon@gmail.com>
2017-01-14 19:18:45 +0100

    some modif for config (pour que le code marche sur malbec)

--- etc/config.h
+++ etc/config.h
@@ -1,8 +1,8 @@
 CC=icc
 #FC=mpif90 
 #FC=mpiifort
-FC=gfortran
-#FC=ifort
+#FC=gfortran
+FC=ifort
 CCFLAGS = -O3
 # for debugging: change -O3 -check nobounds to      -check all -debug -g -O0 -fp-stack-check -traceback -ftrapuv
 
@@ -24,11 +24,11 @@ CCFLAGS = -O3
 # I/O throughput lingers at 2.5 MB/s, with it it can increase to ~44 MB/s
 # However it does not make much of a difference on NFS mounted volumes or with SFS 3.1.1 / Lustre 1.6.7.1
 #FC = mpif90
-FFLAGS = -O4 -check nobounds -xAVX -ftz -assume buffered_io -assume byterecl -vec-report3 -implicitnone -warn truncated_source -warn argument_checking -warn declarations -warn alignments -warn ignore_loc -warn usage -mcmodel=medium -shared-intel
+FFLAGS =   -lmkl_intel_lp64 -lmkl_sequential -lmkl_core -lpthread -lm -O4 -check nobounds -xAVX -ftz -assume buffered_io -assume byterecl  -implicitnone -warn truncated_source -warn argument_checking -warn declarations -warn alignments -warn ignore_loc -warn usage -mcmodel=medium -shared-intel
 
 # useful for debugging:
 #FFLAGS=  -llapack   -g -O0 -traceback
-FFLAGS=  -llapack  -check all -debug -g -O0 -fp-stack-check -traceback -ftrapuv -assume byterecl -L/cm/shared/apps/intel/composer_xe/current/compiler/lib/intel64
+#FFLAGS=  -llapack  -check all -debug -g -O0 -fp-stack-check -traceback -ftrapuv -assume byterecl -L/cm/shared/apps/intel/composer_xe/current/compiler/lib/intel64
 #FFLAGS = -llapack -check all -debug -g
 #FFLAGS=    -llapack -check all -debug -g -O0 -fp-stack-check -traceback -ftrapuv -assume byterecl
 # change    -vec-report0      to      -vec-report3     to get a vectorization report
@@ -38,9 +38,9 @@ FFLAGS=  -llapack  -check all -debug -g -O0 -fp-stack-check -traceback -ftrapuv 
 ################################################
 
 #FC = mpif90
-FFLAGS = -g  -std=gnu  -fimplicit-none -frange-check -O2 -pedantic -pedantic-errors -Waliasing -Wampersand -Wline-truncation -Wsurprising -Wunderflow -fbounds-check -llapack -w
+#FFLAGS = -g  -std=gnu  -fimplicit-none -frange-check -O2 -pedantic -pedantic-errors -Waliasing -Wampersand -Wline-truncation -Wsurprising -Wunderflow -fbounds-check -llapack -w
 
-FFLAGS= -l lapack -w -g  -std=gnu  -fimplicit-none -frange-check -O2 -pedantic -pedantic-errors -Waliasing -Wampersand -Wline-truncation -Wsurprising -Wunderflow -fbounds-check 
+#FFLAGS= -l lapack -w -g  -std=gnu  -fimplicit-none -frange-check -O2 -pedantic -pedantic-errors -Waliasing -Wampersand -Wline-truncation -Wsurprising -Wunderflow -fbounds-check 
 
 #FFLAGS = -llapack   
 
